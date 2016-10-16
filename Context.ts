2b42024df3614aaa25380e0972908f67d27ddb0f
Atsushi Suzuki <possibly.maybe.probably@gmail.com>
2016-10-16 21:28:31 +0900

    initial commit

---
+++ Context.ts
@@ -0,0 +1,216 @@
+const debug = require("debug")("Context");
+
+
+type Lifetime = "singleton"|"context"|"transient";
+
+interface Creator<T> {
+  (this: Context, dependencies: any[], args: any[]): any|Promise<any>;
+}
+
+interface Disposer<T> {
+  (this: Context, instance: T): void|Promise<void>;
+}
+
+interface RegistrationParams<T> {
+  name: string;
+  lifetime?: Lifetime;
+  dependencies?: string[];
+  create: Creator<T>;
+  dispose?: Disposer<T>;
+}
+
+interface Registration {
+  name: string;
+  lifetime: Lifetime;
+  dependencies: string[];
+  create: Creator<any>;
+  dispose?: Disposer<any>;
+  context: Context;
+}
+
+interface Instantiation {
+  createPromise: Promise<any>;
+  disposePromise: Promise<void>|null;
+  registration: Registration;
+  dependencies: Instantiation[];
+  context: Context;
+}
+
+export class Context {
+  private parent: Context|null = null;
+  private children: Context[] = [];
+  private registrations: {[name: string]: Registration} = Object.create(null);
+  private instances: {[name: string]: Instantiation} = Object.create(null);
+
+
+  constructor(public name: string = "") {
+  }
+
+
+  private path() {
+    const nodes: string[] = [];
+    for (let ctx: Context|null = this; ctx !== null; ctx = ctx.parent) {
+      nodes.unshift(ctx.name);
+    }
+    return nodes.join("/");
+  }
+
+  registerTransient<T>(name: string, dependencies: string[], create: Creator<T>) {
+    this.register({
+      name,
+      lifetime: "transient",
+      dependencies,
+      create,
+    });
+  }
+
+  registerContext<T>(name: string, dependencies: string[], create: Creator<T>, dispose?: Disposer<T>) {
+    this.register({
+      name,
+      lifetime: "context",
+      dependencies,
+      create,
+      dispose,
+    });
+  }
+
+  registerSingleton<T>(name: string, dependencies: string[], create: Creator<T>, dispose?: Disposer<T>) {
+    this.register({
+      name,
+      lifetime: "singleton",
+      dependencies,
+      create,
+      dispose,
+    });
+  }
+
+  registerValue<T>(name: string, value: T) {
+    this.register({
+      name,
+      lifetime: "transient",
+      create: () => value,
+    });
+  }
+
+  register<T>({name, lifetime, dependencies, create, dispose}: RegistrationParams<T>): void {
+    if (typeof name !== "string") {
+      throw new TypeError(`"name" must be string`);
+    }
+    if (lifetime !== "singleton" &&
+        lifetime !== "context" &&
+        lifetime !== "transient" &&
+        Boolean(lifetime) !== false) {
+      throw new TypeError(`"lifetime" must be one of "singleton", "context", "transient" or false`);
+    }
+    if (typeof create !== "function") {
+      throw new TypeError(`"create" must be function`);
+    }
+    if (typeof dispose !== "function" && typeof dispose !== "undefined" && dispose !== null) {
+      throw new TypeError(`"create" must be function, null or undefined`);
+    }
+    lifetime = lifetime || "transient";
+    if (lifetime === "transient" && typeof dispose === "function") {
+      throw new TypeError(`registration with lifetime "transient" cannot have dispose function`);
+    }
+
+    debug(`register ${this.path()}/${name}`);
+    this.registrations[name] = {
+      name,
+      lifetime: lifetime,
+      dependencies: Array.from(dependencies || []),
+      create,
+      dispose: dispose || undefined,
+      context: this,
+    };
+  }
+
+  resolve(name: string, ...args: any[]): Promise<any> {
+    return this.resolveImpl(name, args).createPromise;
+  }
+
+  private resolveImpl(name: string, args: any[]): Instantiation {
+    let reg: Registration|undefined;
+    for (let ctx: Context|null = this; ctx !== null; ctx = ctx.parent) {
+      if (name in ctx.registrations) {
+        reg = ctx.registrations[name];
+        break;
+      }
+    }
+    if (!reg) {
+      throw new Error(`registration not found: ${this.path()}/${name}`);
+    }
+
+    const ctx = (reg.lifetime === "singleton") ? reg.context : this;
+    if ((reg.lifetime === "singleton" || reg.lifetime === "context") &&
+        (name in ctx.instances)) {
+      debug(`resolving ${this.path()}/${name} from cache at ${ctx.path()}/${name}`);
+      return ctx.instances[name];
+    }
+
+    debug(`resolving ${this.path()}/${name} with new instance at ${ctx.path()}/${name}`);
+    const inst = this.instantiate(ctx, reg, args);
+    if (reg.lifetime === "singleton" || reg.lifetime === "context") {
+      ctx.instances[name] = inst;
+    }
+    return inst;
+  }
+
+  private instantiate(ctx: Context, reg: Registration, args: any[]): Instantiation {
+    debug(`  creating ${ctx.path()}/${reg.name} with dependencies: [${reg.dependencies.map((name) => `"${name}"`).join(", ")}]`);
+
+    const deps = reg.dependencies.map((name) => ctx.resolveImpl(name, []));
+    return {
+      createPromise: (async () => {
+        const dependencies = await Promise.all(deps.map((dep) => dep.createPromise));
+        const instance = await reg.create.call(ctx, dependencies, args);
+        debug(`  created ${ctx.path()}/${reg.name}: ${inspect(instance)}`);
+        return instance;
+      })(),
+      disposePromise: null,
+      registration: reg,
+      dependencies: deps,
+      context: ctx,
+    };
+  }
+
+  childContext(name?: string): Context {
+    const child = new Context(name || `child[${this.children.length}]`);
+    child.parent = this;
+    this.children.push(child);
+    return child;
+  }
+
+  async destroy(): Promise<void> {
+    debug(`destroying ${this.path() || "/"}`);
+    await Promise.all(this.children.map((ctx) => ctx.destroy()));
+    await Promise.all(Object.keys(this.instances).map((name) => this.dispose(this.instances[name])));
+  }
+
+  private dispose(inst: Instantiation): Promise<void> {
+    if (inst.disposePromise) {
+      return inst.disposePromise;
+    } else {
+      return inst.disposePromise = (async () => {
+        const dependants = Object.keys(this.instances)
+          .map((name) => this.instances[name])
+          .filter((x) => x.dependencies.indexOf(inst) !== -1);
+
+        debug(`  disposing ${inst.context.path()}/${inst.registration.name} with dependants: [${dependants.map((x) => `"${x.registration.name}"`).join(", ")}]`);
+        
+        await Promise.all(dependants.map((x) => this.dispose(x)));
+        
+        if (inst.registration.dispose) {
+          await inst.registration.dispose.call(this, await inst.createPromise);
+        }
+      })();
+    }
+  }
+}
+
+function inspect(obj: any) {
+  if (typeof obj === "object" && obj !== null && typeof obj.constructor === "function") {
+    return `[object ${obj.constructor.name}]`;
+  } else {
+    return Object.prototype.toString.call(obj);
+  }
+}
---
+++ test/Context.test.ts
@@ -0,0 +1,219 @@
+import * as assert from "assert";
+import {Context} from "../Context";
+
+
+describe("Context", () => {
+  it("simple", async () => {
+    const ctx = new Context();
+    ctx.register({
+      name: "hello",
+      create() {
+        return "world";
+      }
+    });
+
+    const actual = await ctx.resolve("hello");
+    assert.strictEqual(actual, "world");
+  });
+
+  it("dependency resolution", async () => {
+    const ctx = new Context();
+    ctx.register({
+      name: "greeting",
+      dependencies: ["name"],
+      create([name]: [string]) { return `Hello, ${name}!`; },
+    });
+    ctx.register({
+      name: "name",
+      create() { return "world"; }
+    });
+
+    const actual = await ctx.resolve("greeting");
+    assert.strictEqual(actual, "Hello, world!");
+  });
+
+  it("asynchronous creation", async () => {
+    const ctx = new Context();
+    ctx.register({
+      name: "hello",
+      async create() {
+        await wait(20);
+        return "world";
+      }
+    });
+
+    const actual = await ctx.resolve("hello");
+    assert.strictEqual(actual, "world");
+  });
+
+  it("transient lifetime", async () => {
+    const ctx = new Context();
+    let called = 0;
+    ctx.register({
+      name: "hello",
+      async create() {
+        called++;
+        return "world";
+      },
+      lifetime: "transient",
+    });
+
+    const actual = await Promise.all([ctx.resolve("hello"), ctx.resolve("hello")]);
+    assert.deepStrictEqual(actual, ["world", "world"]);
+    assert.strictEqual(called, 2);
+  });
+
+  it("context lifetime", async () => {
+    const ctx = new Context();
+    let called = 0;
+    ctx.register({
+      name: "hello",
+      async create() {
+        called++;
+        await wait(20);
+        return "world";
+      },
+      lifetime: "context",
+    });
+
+    const actual = await Promise.all([ctx.resolve("hello"), ctx.resolve("hello")]);
+    assert.deepStrictEqual(actual, ["world", "world"]);
+    assert.strictEqual(called, 1);
+  });
+
+  it("singleton lifetime", async () => {
+    const ctx = new Context();
+    let called = 0;
+    ctx.register({
+      name: "hello",
+      async create() {
+        called++;
+        await wait(20);
+        return "world";
+      },
+      lifetime: "singleton",
+    });
+
+    const actual = await Promise.all([ctx.resolve("hello"), ctx.resolve("hello")]);
+    assert.deepStrictEqual(actual, ["world", "world"]);
+    assert.strictEqual(called, 1);
+  });
+
+  it("destroy", async () => {
+    const ctx = new Context();
+    let create = 0, destroy = 0;
+    let instance: any = undefined;
+    ctx.register({
+      name: "hello",
+      async create() {
+        create++;
+        return "world";
+      },
+      async dispose(v: any) {
+        instance = v;
+        destroy++;
+      },
+      lifetime: "context",
+    });
+
+    const actual = await ctx.resolve("hello");
+    assert.strictEqual(actual, "world");
+    assert.strictEqual(create, 1);
+    assert.strictEqual(destroy, 0);
+
+    await ctx.destroy();
+    assert.strictEqual(instance, "world");
+    assert.strictEqual(create, 1);
+    assert.strictEqual(destroy, 1);
+  });
+
+  it("destroy order", async () => {
+    const ctx = new Context();
+    const check: string[] = [];
+    ctx.register({
+      name: "name",
+      async create() { return "world"; },
+      async dispose(v: any) {
+        check.push("c");
+        await wait(20);
+        check.push("d");
+      },
+      lifetime: "context",
+    });
+    ctx.register({
+      name: "greet",
+      dependencies: ["name"],
+      async create([name]: [string]) { return `Hello, ${name}!`; },
+      async dispose(v: any) {
+        check.push("a");
+        await wait(20);
+        check.push("b");
+      },
+      lifetime: "context",
+    });
+
+    const actual = await ctx.resolve("greet");
+    assert.strictEqual(actual, "Hello, world!");
+
+    await ctx.destroy();
+    assert.deepStrictEqual(check, ["a", "b", "c", "d"]);
+  });
+
+  it("child context, transient", async () => {
+    const ctx = new Context();
+    ctx.register({
+      name: "hello",
+      create() {
+        return "world";
+      }
+    });
+    const child = ctx.childContext();
+
+    const actual = await child.resolve("hello");
+    assert.strictEqual(actual, "world");
+  });
+
+  it("child context, context", async () => {
+    const ctx = new Context();
+    ctx.register({
+      name: "hello",
+      create() {
+        return {};
+      },
+      lifetime: "context",
+    });
+    const child1 = ctx.childContext();
+    const child2 = ctx.childContext();
+
+    const actual1 = await child1.resolve("hello");
+    assert.deepStrictEqual(actual1, {});
+    const actual2 = await child2.resolve("hello");
+    assert.deepStrictEqual(actual2, {});
+    assert.notStrictEqual(actual1, actual2);
+  });
+
+  it("child context, singleton", async () => {
+    const ctx = new Context();
+    ctx.register({
+      name: "hello",
+      create() {
+        return {};
+      },
+      lifetime: "singleton",
+    });
+    const child1 = ctx.childContext();
+    const child2 = ctx.childContext();
+
+    const actual1 = await child1.resolve("hello");
+    assert.deepStrictEqual(actual1, {});
+    const actual2 = await child2.resolve("hello");
+    assert.deepStrictEqual(actual2, {});
+    assert.strictEqual(actual1, actual2);
+  });
+});
+
+function wait(ms: number): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
+}
